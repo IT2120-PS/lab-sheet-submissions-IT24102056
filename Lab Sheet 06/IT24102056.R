cff8aaee9cd47fb23cd84c8b166684d19330c739
Rohansie K.D.S. <it24102056@my.sliit.lk>
2025-09-05 11:42:17 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24102056.R
@@ -0,0 +1,24 @@
+setwd("C:\\Users\\user\\Desktop\\IT24102056")
+
+#Exercise
+#1
+
+#i)
+#Binomial Distribution
+#Here, Random variable x has binomial distribution with n=50 and p=0.85
+
+#ii)
+pbinom(47,50,0.85, lower.tail = FALSE)
+
+
+#2
+
+#i)
+#Customer calls per hour
+
+#ii)
+#Poisson distribution
+#Here, random variable x has Poisson distribution with lamda = 12
+
+#iii)
+dpois(15,12)
