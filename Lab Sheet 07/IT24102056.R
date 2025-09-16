6cb9a405192051f721b54d9187a5bf05c5db5128
Rohansie K.D.S. <it24102056@my.sliit.lk>
2025-09-16 11:59:50 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102056.R
@@ -0,0 +1,16 @@
+setwd("C:\\Users\\user\\Desktop\\IT24102056")
+
+#Exercise
+#1
+#P(10<=X<=25)
+punif(25, min = 0, max = 40, lower.tail = TRUE) - punif(10, min = 0, max = 40, lower.tail = TRUE)
+
+#2
+pexp(2, rate = 1/3, lower.tail = TRUE)
+
+#3
+#i)
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+#ii)
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
