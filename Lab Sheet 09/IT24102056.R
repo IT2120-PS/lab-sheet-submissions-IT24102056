86380f8cb292ddc27ea32b099c5f0c6058b49493
Rohansie K.D.S. <it24102056@my.sliit.lk>
2025-10-01 16:32:45 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT24102056.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\user\\Desktop\\IT24102056")
+getwd()
+
+#Generate a random sample of size 25 for the baking time.
+baking_time <- rnorm(25, mean = 45, sd = 2)
+baking_time
+
+#Test whether the average baking time is less than 46 minutes at a 5% level of
+#significance.
+t.test(baking_time, mu = 46, alternative = "less")
+
+res <- t.test(baking_time, mu = 46, alternative = "less")
+res$statistic   
+res$p.value     
+res$conf.int  
